11722e2804846b98a3348ad6fadeb6e0b9a7209b
Rinold Simon <crisrinold@gmail.com>
2019-06-02 16:47:11 +0530

    add pagination, tweak if no results

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
 import config from "../config/environment";
 import $ from 'jquery';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
+import { alias } from '@ember/object/computed';
+import { oneWay } from '@ember/object/computed';
+import { run } from '@ember/runloop';
 
 export default Controller.extend({
-  ajax: service(),
+  initialLoad: true,
+  searching: false,
   // url: 'https://www.goodreads.com/search/index.xml',
   url: 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search.xml',
   // Changes XML to JSON
@@ -48,20 +52,53 @@ export default Controller.extend({
     }
     return obj;
   },
+  content: '',
+  // setup our query params
+  queryParams: ["page", "perPage"],
+
+  // set default values, can cause problems if left out
+  // if value matches default, it won't display in the URL
+  page: 1,
+  perPage: 10,
+
+  // iterate over pagedContent in your template
+  pagedContent: pagedArray('content', {
+    page: alias("parent.page"),
+    perPage: alias("parent.perPage")
+  }),
+
+  // binding the property on the paged array
+  // to a property on the controller
+  totalPages: oneWay("pagedContent.totalPages"),
+
   actions: {
     search(searchTerm) {
+      this.set('searching', true);
       $.ajax(this.get('url'), {
         data: {
           q: searchTerm,
           key: config.api_key,
-          'search[field]': 'title'
+          'search[field]': 'title',
+          // page: 2
         },
         headers: {
           "X-Requested-With": "XMLHttpRequest"
         }
       }).then((xmlData) => {
         let jsonData = this.xmlToJson(xmlData)
-        debugger;
+        run(() => {
+          this.set('page', 1);
+          this.set('initialLoad', false);
+          this.set('searchTermCopy', searchTerm);
+          this.set('searching', false);
+          let data = jsonData.GoodreadsResponse.search.results.work;
+          data.forEach((data) => {
+            if (data.original_publication_year["@attributes"]) {
+              data.original_publication_year = ''
+            }
+          });
+          this.set('content', data);
+        })
       });
     }
   }
--- app/services/ajax.js
+++
@@ -1,8 +0,0 @@
-import AjaxService from 'ember-ajax/services/ajax';
-import { computed } from '@ember/object';
-
-export default AjaxService.extend({
-  trustedHosts: computed(function() {
-    return ['https://www.goodreads.com']
-  })
-});
